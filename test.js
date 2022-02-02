1a9738084697c5c3571425dda2a0a401189a8d90
bbakerweb <bbakerweb@users.noreply.github.com>
2022-02-02 10:15:36 -0800

    Added javascript functionality

---
+++ test.js
@@ -0,0 +1,8 @@
+function addSum(first,last){
+	total = first+last;
+	return total;
+}
+
+firstName = prompt("Enter a First name");
+lastName = prompt("Enter a last name");
+addSum(firstName,lastName);
